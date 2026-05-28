0810728016518e6a4fc3af2d9ecf6de8561e35b3
AriPanque <116276066+Ari-px@users.noreply.github.com>
2026-05-28 16:01:02 -0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
-export const connectDB = async () => {
+const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);
 
-    console.log(`MongoDB conectado: ${connection.connection.host}`);
+    console.log("MongoDB conectado");
   } catch (error) {
-    console.error("Error al conectar MongoDB:", error.message);
+    console.log("Error MongoDB:", error.message);
     process.exit(1);
   }
 };
+
+export default connectDB;
